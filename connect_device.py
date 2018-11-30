45bb9bf3fea180db16efd0bc687eb3fc6c998eac
Qixin Dong <qixidong@pensando.io>
2018-11-29 16:11:20 -0800

    minor fix

--- connect_device.py
+++ connect_device.py
@@ -6,7 +6,7 @@ ip = sys.argv[1]
 username = sys.argv[2]
 password = sys.argv[3]
 
-expect_list = ['continue connecting (yes/no)? $', 'Password: $', 'Password for', 'password: $',pexpect.EOF,pexpect.TIMEOUT]
+expect_list = ['continue connecting \(yes\/no\)\? $', 'Password: $', 'Password for', 'password: $',pexpect.EOF,pexpect.TIMEOUT]
 hdl = pexpect.spawn('ssh %s@%s'%(username,ip))
 for i in range(5):
     i = hdl.expect(expect_list, timeout = 5)
@@ -19,4 +19,6 @@ for i in range(5):
         break
     if i == 4 or i == 5:
         sys.exit("target device seems unavailable right now")
+hdl.setwinsize(40,200)
+#print(hdl.getwinsize())
 hdl.interact()
